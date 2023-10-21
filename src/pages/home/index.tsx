38d867f21552ad801c0b11c01b29f7571b535c7a
CaioAlmeida09 <Caio09092001@gmail.com>
2023-10-21 10:18:28 -0300

    criando a página login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -22,7 +22,10 @@ export function Home() {
       </h1>
       <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         <section className=" flex flex-col px-3 py-2 items-center w-full bg-white">
-          <img src={ney} />
+          <img
+            src={ney}
+            className="w-full max-h-72 hover:scale-105 transition-all"
+          />
           <h2 className="text-black font-bold text-lg mt-6"> Nome do Carro </h2>
           <div className="mt-3 flex flex-col gap-8 mb-3 items-center">
             <p> 2001/2002 | 87987km</p>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,31 @@
+import { Container } from "../../components/container";
+import logo from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 export function Login() {
   return (
-    <div>
-      <h1>Hello Login</h1>
-    </div>
+    <Container>
+      <section className="flex flex-col w-full h-screen justify-start mt-16 items-center ">
+        <main className="flex flex-col w-3/4 max-w-2xl md:px-6 md:py-4 px-2 py-4 gap-7 bg-white drop-shadow items-center">
+          <img src={logo} alt="logo" />
+          <input
+            className="w-full h-10 rounded-lg border-2 px-2 text-zinc-900"
+            placeholder="Digite seu e-mail..."
+          />
+          <input
+            className="w-full h-10 rounded-lg border-2 px-2 "
+            placeholder="******"
+          />
+          <button className="bg-black w-full h-9 rounded-lg text-white font-medium">
+            {" "}
+            Acessar{" "}
+          </button>
+          <Link to="/register">
+            <p className="text-zinx-900 text-xs font-medium hover:font-bold">
+              Ainda não possui uma conta? Cadastra-se
+            </p>
+          </Link>
+        </main>
+      </section>
+    </Container>
   );
 }
